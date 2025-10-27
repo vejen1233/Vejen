1de455b12a385534d535777035c8a6b8b831dd10
vejen1233 <mariavejenfarbaek@hotmail.com>
2025-10-27 22:11:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: '/vejen',
   assetPrefix: '/vejen/',
 };
+module.exports = nextConfig;
